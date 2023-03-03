cf771f3b4ee9523d036250f4ad1849b916441bbf
eugeniarossi <eugenia.rossi@outlook.com>
2023-03-03 18:45:48 +0100

    function start game

--- js/main.js
+++ js/main.js
@@ -22,47 +22,57 @@ function myAddClass(htmlElement, className) {
     htmlElement.classList.add(className);
 }
 
-function startGame() {
-    // Definisco una variabile per il numero di elementi
-    let cellNumber = 0;
-    // Svuoto la board
-    board.innerHTML = '';
-    // Leggo il valore dell'input select
-    const level = selectInput.value;
-    // Definisco una variabile livello per le classi css
-    let levelClass;
+// Definisco una funzione per leggere il valore di un input
+function inputValue(input) {
+    const inputValue = input.value;
+    return inputValue;
+}
 
+// Definisco una funzione per calcolare il numero di celle
+function calcCellsNumber(level) {
+    // Definisco una variabile per il numero di elementi
+    let cellsNumber = 0;
     // Stabilisco delle condizioni
     if (level === 'hard') {
-        cellNumber = 49;
-        levelClass = 'hard';
+        cellsNumber = 49;
     } else if (level === 'medium') {
-        cellNumber = 81;
-        levelClass = 'medium';
+        cellsNumber = 81;
     } else {
-        cellNumber = 100;
-        levelClass = 'easy';
+        cellsNumber = 100;
     }
+    return cellsNumber;
+}
+
+// Definisco una funzione per creare le celle
+function createBoard(level, cellsNumber) {
+    // Svuoto la board
+    board.innerHTML = '';
 
     // Creo un document fragment
     let fragment = document.createDocumentFragment();
     // Creo gli elementi
-    for (let i = 1; i <= cellNumber; i++) {
+    for (let i = 1; i <= cellsNumber; i++) {
         const createdElement = myCreateElement('div', 'cell', i);
         // Assegno la classe livello
-        myAddClass(createdElement, levelClass);
-        createdElement.addEventListener('click', function () {
+        myAddClass(createdElement, level);
+        createdElement.addEventListener('click', function() {
             // Definisco il comportamento dell' elemento al click
             myAddClass(createdElement, 'cell-onclick');
-            console.log(createdElement.innerText);
+            pushNotIncluded(clickedCellsList, i);
+            console.log({clickedCellsList});
         });
         // Aggiungo l'elemento al frammento
         myAppend(fragment, createdElement);
     }
     // Aggiungo il frammento alla board
     myAppend(board, fragment);
-    // Calcolo il punteggio necessario per vincere
-    victoryScore = cellNumber - bombsNumber;
+    return cellsNumber;
+}
+
+function pushNotIncluded(myArray, myNumber) {
+    if (!myArray.includes(myNumber)) {
+        myArray.push(myNumber);
+    }
 }
 
 // Definisco una funzione che generi un array con numeri random senza ripetizioni
@@ -70,9 +80,7 @@ function generateRandomNums(randomNumsArray, arrayLength, maxNum) {
     randomNumsArray = [];
     while (randomNumsArray.length < arrayLength) {
         const generatedNumber = Math.floor(Math.random() * maxNum) + 1;
-        if (!randomNumsArray.includes(generatedNumber)) {
-            randomNumsArray.push(generatedNumber);
-        }
+        pushNotIncluded(randomNumsArray, generatedNumber);
     }
     return randomNumsArray;
 }
@@ -83,16 +91,40 @@ function generateRandomNums(randomNumsArray, arrayLength, maxNum) {
 
 // START GAME
 
+function startGame() {
+    // Leggo il livello
+    const level = inputValue(selectInput);
+    // Calcolo il numero di celle
+    let cellsNumber = calcCellsNumber(level);
+    // Creo la board
+    createBoard(level, cellsNumber);
+    // Creo un array con le bombe
+    bombs = generateRandomNums('bombs', bombsNumber, cellsNumber);
+    // Calcolo il punteggio per la vittoria
+    victoryScore = cellsNumber - bombsNumber;
+}
+
 // Definisco delle variabili in cui salvo board, button, select
 const board = document.querySelector('.board');
 const playButton = document.querySelector('.play-button');
 const selectInput = document.getElementById('level');
-
 const bombsNumber = 16;
-let victoryScore = 0;
-// Aggiungo un evento onclick al play button
-playButton.addEventListener('click', startGame);
 
+let bombs;
+let victoryScore = 0;
+let clickedCellsList = [];
+
+// Aggiungo un evento onclick al play button
+
+playButton.addEventListener('click', function() {
+    
+    startGame();
+});
 // GAME LOGIC
 
-generateRandomNums('bombs', 16, 100);
+// Calcolo il punteggio necessario per vincere
+/*
+
+
+generateRandomNums('bombs', bombsNumber, cellsNumber);
+*/
